bf2e977da098e867eaa9f08b0b9cb4e08686b0e8
urządzenia programy <dannorbi@outlook.com>
2020-07-10 12:23:21 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace instrif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj cene: ");
+            int cena = int.Parse(Console.ReadLine());
+            if (cena > 400)
+            {
+                cena -= 80;
+                Console.WriteLine("rabat to 80");
+
+
+            }
+            else
+            {
+                if (cena > 200)
+                {
+                    cena -= 35;
+                    Console.WriteLine("rabat 35");
+                }
+                else
+                {
+                    if (cena > 100)
+                    {
+                        cena -= 15;
+                        Console.WriteLine("rabat 15");
+
+                    }
+                    else { Console.WriteLine("brak rabatu"); }
+                }
+               
+
+            }
+
+            Console.WriteLine("cena koncowa: ");
+            Console.WriteLine(cena);
+        }
+
+    }
+}
